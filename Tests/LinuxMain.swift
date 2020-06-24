d503d5dff55abf080b811e03ef094def27411e1e
hanchen <hanchen@vxiaoke360.com>
2020-06-24 14:10:40 +0800

    Package.swift

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import RTRootNavigationControllerTests
+
+var tests = [XCTestCaseEntry]()
+tests += RTRootNavigationControllerTests.allTests()
+XCTMain(tests)
---
+++ Tests/RTRootNavigationControllerTests/RTRootNavigationControllerTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import RTRootNavigationController
+
+final class RTRootNavigationControllerTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RTRootNavigationController().text, "Hello, World!")
+    }
+
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
